4e66fdd0b0debb570802de5c9ad10bd755b5962a
Ginni Pinckert <gcpinckert@gmail.com>
2023-03-09 21:35:01 -0800

    routes for tests

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const pg = require("./models/pg_client")
-const tests = require("./models/tests")
 
 // connect to database
 pg.checkConnection()
 
 // logging
 
+const cors = require("cors")
+app.use(cors())
+
 // router API
+const testsController = require('./controllers/tests')
+app.use("/tests", testsController)
 
 // path not found and error middlewares
 
---
+++ controllers/tests.js
@@ -0,0 +1,25 @@
+const tests = require('../models/tests')
+
+const express = require('express');
+const { response } = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const test_ids = await tests.get()
+    res.status(200).json(test_ids)
+  } catch (error) {
+    res.status(500).send(error);
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const id = await tests.create()
+    res.status(201).json(id)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+module.exports = router;
